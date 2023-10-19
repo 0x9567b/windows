168ef3801c0cabd912d103e9e3908506a64577d3
Paul <72038577+0xP4UL@users.noreply.github.com>
2023-10-19 16:46:44 +0100

    Update script.ps1
    Added more current user locations.

--- script.ps1
+++ script.ps1
@@ -2,6 +2,11 @@
 $locations = @(
     "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
     "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServices",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce",
+    "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Windows\Load",
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Windows\Scripts",
 )
 
 
