73e0ee744c02f65317b7934f0bfc4dcc3fdb0b50
Paul <72038577+0xP4UL@users.noreply.github.com>
2023-10-19 16:56:03 +0100

    Update script.ps1
    Added local machine locations.

--- script.ps1
+++ script.ps1
@@ -1,5 +1,6 @@
 
 $locations = @(
+    # Current User
     "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
     "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
     "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunServices",
@@ -7,6 +8,13 @@ $locations = @(
     "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows",
     "HKCU:\Software\Microsoft\Windows\CurrentVersion\Windows\Load",
     "HKCU:\Software\Microsoft\Windows\CurrentVersion\Windows\Scripts",
+
+    # Local Machine
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run",
+    "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce",
+    "HKLM\Software\Microsoft\Windows\CurrentVersion\RunServices",
+    "HKLM\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce",
+    "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Userinit"
 )
 
 
